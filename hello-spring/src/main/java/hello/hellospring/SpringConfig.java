4582fe6cc84345c47027492a5def9aa6b984e25c
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2022-01-14 23:10:41 +0900

    Spring DB approach tech
    Install H2 Database, experience JDBC, JPA and Spring Data JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,23 +1,37 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+   /* @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
-
+*/
 
 }
