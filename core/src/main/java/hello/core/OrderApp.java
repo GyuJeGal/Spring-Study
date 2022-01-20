4ac273f1b38a92c6eeeecc329a2ff20ba768e435
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2022-01-20 23:51:01 +0900

    Spring container and bean
    스프링 컨테이너 생성, 스프링 빈 조회 - Basic

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -21,7 +21,7 @@ public class OrderApp {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
-        OrderService orderService = applicationContext.getBean("orderService", OrderService.class); 
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member1", Grade.VIP);
