117c09884af9cfd7e44510fa6a9187c433fd4324
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2022-01-30 23:07:14 +0900

    Bean Life Cycle CallBack
    빈 등록 초기화, 소멸 메서드

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifecycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean(initMethod = "init", destroyMethod = "close") //destroyMethod에는 default 값으로 (inferred)가 등록되어 있음.
+                                                            //close나 shutdown이라는 이름의 메서드를 자동으로 호출.
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
