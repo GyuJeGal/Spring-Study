b740e3be47e34da458b8c0e2de5c096840834b2d
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2022-02-27 16:58:49 +0900

    상품 상세

--- spring-MVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-MVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,6 +28,16 @@ public class BasicItemController {
 
        return "basic/items";
    }
+
+   @GetMapping("/{itemId}")
+   public String item(@PathVariable long itemId, Model model) {
+
+       Item item = itemRepository.findById(itemId);
+       model.addAttribute("item", item);
+       return "basic/item";
+
+   }
+
 
    @PostConstruct
     public void init() {
