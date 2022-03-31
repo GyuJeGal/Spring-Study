4d55bb2fb2377b9bfceb54452f675ed9d3e4802d
GyuJeGal <69112154+GyuJeGal@users.noreply.github.com>
2022-03-31 17:29:12 +0900

    Entity 클래스 개발2

--- jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Item/Item.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop.domain.Item;
 
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -21,6 +24,7 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
